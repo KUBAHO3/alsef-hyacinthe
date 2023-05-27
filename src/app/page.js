4c43302115997c61071f1f20f8b22ba3a85fef09
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-05-27 18:45:23 +0530

    Add contact section

--- src/app/page.js
+++ src/app/page.js
@@ -143,15 +143,15 @@ export default function Home() {
               <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">The Forum is committed to enhancing life science research in Africa by providing a platform for knowledge exchange, networking, and collaboration.</p>
             </div>
             <div className="flex flex-col p-2 justify-between leading-normal bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-              <h6 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Strengthen Innovation Ecosystems</h6>
+              <h6 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Strengthen Innovation Ecosystems</h6>
               <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">The Forum strengthens African innovation ecosystems through research partnerships, capacity building, and policy advocacy.</p>
             </div>
             <div className="flex flex-col p-2 justify-between leading-normal bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-              <h6 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Promote Science-Based Entrepreneurship</h6>
+              <h6 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Promote Science-Based Entrepreneurship</h6>
               <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">The Forum empowers young African life scientists and innovators to become science-based entrepreneurs through mentorship, training, and support.</p>
             </div>
             <div className="flex flex-col p-2 justify-between leading-normal bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-              <h6 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Empower the Next Generation of African Life Scientists</h6>
+              <h6 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Empower the Next Generation of African Life Scientists</h6>
               <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">The Forum aims to support young African life scientists by offering skill-building, knowledge sharing, and exposure to the latest research and innovation.</p>
             </div>
           </div>
@@ -159,15 +159,15 @@ export default function Home() {
         <div className="flex items-center justify-center w-full h-auto flex-wrap max-w-screen-2xl mx-auto my-8 content-center bg-[url('https://images.pexels.com/photos/4386466/pexels-photo-4386466.jpeg')]">
           <div className="w-9/12">
             <div className="flex flex-col p-2 justify-between leading-normal bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-              <h6 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Increase Research Funding in Africa</h6>
+              <h6 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Increase Research Funding in Africa</h6>
               <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">The Forum supports African research funding by advocating for investment in life sciences research and facilitating the development of grant proposals and public-private partnerships.</p>
             </div>
             <div className="flex flex-col p-2 justify-between leading-normal bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-              <h6 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Strengthen Institutional Partnerships</h6>
+              <h6 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Strengthen Institutional Partnerships</h6>
               <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">The Forum strengthens institutional partnerships between African and international universities, research institutions, and industry partners to promote innovation and knowledge exchange.</p>
             </div>
             <div className="flex flex-col p-2 justify-between leading-normal bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-              <h6 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Promote Stem Education in Africa</h6>
+              <h6 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Promote Stem Education in Africa</h6>
               <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">The Forum supports STEM education in Africa by providing educational resources and support to schools and universities, and organizing STEM programs and competitions for students.</p>
             </div>
           </div>
@@ -226,7 +226,61 @@ export default function Home() {
             </a>
           </div>
         </div>
-      </section> 
+      </section>
+      <section className="py-8">
+        <span className="font-semibold text-gray-400 flex items-center justify-center pb-8 uppercase">Contact us</span>
+        <div className="px-4 w-full max-w-screen-xl mx-auto content-center">
+          <iframe style={{border:0, width: "100%", height: '270px' }} src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621" frameborder="0" allowfullscreen></iframe>
+        </div>
+        <div className="flex items-center justify-center w-full flex-wrap max-w-screen-xl mx-auto px-3 my-4 content-center ">
+          <div className="w-4/12 pr-4">
+            <div className="relative px-4 py-3 my-3 bg-white ring-1 ring-gray-900/5 rounded-lg leading-none flex items-top justify-start space-x-6">
+              <svg className="w-8 h-8 text-purple-600" fill="none" viewBox="0 0 24 24">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M6.75 6.75C6.75 5.64543 7.64543 4.75 8.75 4.75H15.25C16.3546 4.75 17.25 5.64543 17.25 6.75V19.25L12 14.75L6.75 19.25V6.75Z"></path>
+              </svg>
+              <div className="space-y-2">
+                <h6 className="text-slate-800 font-semibold">Location:</h6>
+                <p>Musanze, Street, NR 4 RD, Rwanda</p>
+              </div>
+            </div>
+            <div className="relative px-4 py-3 my-3 bg-white ring-1 ring-gray-900/5 rounded-lg leading-none flex items-top justify-start space-x-6">
+              <svg className="w-8 h-8 text-purple-600" fill="none" viewBox="0 0 24 24">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M6.75 6.75C6.75 5.64543 7.64543 4.75 8.75 4.75H15.25C16.3546 4.75 17.25 5.64543 17.25 6.75V19.25L12 14.75L6.75 19.25V6.75Z"></path>
+              </svg>
+              <div className="space-y-2">
+                <h6 className="text-slate-800 font-semibold">Email:</h6>
+                <p>htuyubahe@gmail.com</p>
+              </div>
+            </div>
+            <div className="relative px-4 py-3 my-3 bg-white ring-1 ring-gray-900/5 rounded-lg leading-none flex items-top justify-start space-x-6">
+              <svg className="w-8 h-8 text-purple-600" fill="none" viewBox="0 0 24 24">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M6.75 6.75C6.75 5.64543 7.64543 4.75 8.75 4.75H15.25C16.3546 4.75 17.25 5.64543 17.25 6.75V19.25L12 14.75L6.75 19.25V6.75Z"></path>
+              </svg>
+              <div className="space-y-2">
+                <h6 className="text-slate-800 font-semibold">Call:</h6>
+                <p>+1 5589 55488 55</p>
+              </div>
+            </div>
+          </div>
+          <div className="w-8/12 pl-4">
+            <div className="py-3 lg:py-3">
+                <form action="#" className="space-y-3">
+                    <div>
+                        <input type="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="name@flowbite.com" required></input>
+                    </div>
+                    <div>
+                        <input type="text" id="subject" className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Let us know how we can help you" required></input>
+                    </div>
+                    <div className="sm:col-span-2">
+                        <textarea id="message" rows="3" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Leave a comment..."></textarea>
+                    </div>
+                    <button type="submit" className="py-3 px-5 text-sm font-medium text-center text-gray-900 rounded-lg border border-gray-300 bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Send message</button>
+                </form>
+            </div>
+          </div>
+        </div>
+      </section>   
+      
     </>
   )
 }
