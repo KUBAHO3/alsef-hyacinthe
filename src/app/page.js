e3178fbcfc9954b8388e153974b0e3972c4a5383
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-05-27 16:28:13 +0530

    Add services section

--- src/app/page.js
+++ src/app/page.js
@@ -138,37 +138,37 @@ export default function Home() {
             </h4>
           </div>
           <div className="w-9/12">
-            <div class="flex flex-col p-2 justify-between leading-normal bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-              <h6 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Enhance African Life Science Research</h6>
-              <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">The Forum is committed to enhancing life science research in Africa by providing a platform for knowledge exchange, networking, and collaboration.</p>
+            <div className="flex flex-col p-2 justify-between leading-normal bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+              <h6 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Enhance African Life Science Research</h6>
+              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">The Forum is committed to enhancing life science research in Africa by providing a platform for knowledge exchange, networking, and collaboration.</p>
             </div>
-            <div class="flex flex-col p-2 justify-between leading-normal bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-              <h6 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Strengthen Innovation Ecosystems</h6>
-              <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">The Forum strengthens African innovation ecosystems through research partnerships, capacity building, and policy advocacy.</p>
+            <div className="flex flex-col p-2 justify-between leading-normal bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+              <h6 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Strengthen Innovation Ecosystems</h6>
+              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">The Forum strengthens African innovation ecosystems through research partnerships, capacity building, and policy advocacy.</p>
             </div>
-            <div class="flex flex-col p-2 justify-between leading-normal bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-              <h6 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Promote Science-Based Entrepreneurship</h6>
-              <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">The Forum empowers young African life scientists and innovators to become science-based entrepreneurs through mentorship, training, and support.</p>
+            <div className="flex flex-col p-2 justify-between leading-normal bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+              <h6 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Promote Science-Based Entrepreneurship</h6>
+              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">The Forum empowers young African life scientists and innovators to become science-based entrepreneurs through mentorship, training, and support.</p>
             </div>
-            <div class="flex flex-col p-2 justify-between leading-normal bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-              <h6 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Empower the Next Generation of African Life Scientists</h6>
-              <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">The Forum aims to support young African life scientists by offering skill-building, knowledge sharing, and exposure to the latest research and innovation.</p>
+            <div className="flex flex-col p-2 justify-between leading-normal bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+              <h6 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Empower the Next Generation of African Life Scientists</h6>
+              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">The Forum aims to support young African life scientists by offering skill-building, knowledge sharing, and exposure to the latest research and innovation.</p>
             </div>
           </div>
         </div>
         <div className="flex items-center justify-center w-full h-auto flex-wrap max-w-screen-2xl mx-auto my-8 content-center bg-[url('https://images.pexels.com/photos/4386466/pexels-photo-4386466.jpeg')]">
           <div className="w-9/12">
-            <div class="flex flex-col p-2 justify-between leading-normal bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-              <h6 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Increase Research Funding in Africa</h6>
-              <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">The Forum supports African research funding by advocating for investment in life sciences research and facilitating the development of grant proposals and public-private partnerships.</p>
+            <div className="flex flex-col p-2 justify-between leading-normal bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+              <h6 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Increase Research Funding in Africa</h6>
+              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">The Forum supports African research funding by advocating for investment in life sciences research and facilitating the development of grant proposals and public-private partnerships.</p>
             </div>
-            <div class="flex flex-col p-2 justify-between leading-normal bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-              <h6 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Strengthen Institutional Partnerships</h6>
-              <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">The Forum strengthens institutional partnerships between African and international universities, research institutions, and industry partners to promote innovation and knowledge exchange.</p>
+            <div className="flex flex-col p-2 justify-between leading-normal bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+              <h6 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Strengthen Institutional Partnerships</h6>
+              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">The Forum strengthens institutional partnerships between African and international universities, research institutions, and industry partners to promote innovation and knowledge exchange.</p>
             </div>
-            <div class="flex flex-col p-2 justify-between leading-normal bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-              <h6 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Promote Stem Education in Africa</h6>
-              <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">The Forum supports STEM education in Africa by providing educational resources and support to schools and universities, and organizing STEM programs and competitions for students.</p>
+            <div className="flex flex-col p-2 justify-between leading-normal bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+              <h6 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Promote Stem Education in Africa</h6>
+              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">The Forum supports STEM education in Africa by providing educational resources and support to schools and universities, and organizing STEM programs and competitions for students.</p>
             </div>
           </div>
           <div className="w-3/12">
@@ -178,6 +178,55 @@ export default function Home() {
           </div>
         </div>
       </section>
+      <section className="py-8">
+        <span className="font-semibold text-gray-400 flex items-center justify-center pb-8 uppercase">Our services</span>
+        <div className="grid grid-cols-1 sm:grid-cols-2 px-4 w-full max-w-screen-xl mx-auto content-center gap-x-8 gap-y-4">
+          <div className="col-span-1 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <svg className="w-10 h-10 mb-2 text-gray-500 dark:text-gray-400" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5 5a3 3 0 015-2.236A3 3 0 0114.83 6H16a2 2 0 110 4h-5V9a1 1 0 10-2 0v1H4a2 2 0 110-4h1.17C5.06 5.687 5 5.35 5 5zm4 1V5a1 1 0 10-1 1h1zm3 0a1 1 0 10-1-1v1h1z" clip-rule="evenodd"></path><path d="M9 11H3v5a2 2 0 002 2h4v-7zM11 18h4a2 2 0 002-2v-5h-6v7z"></path></svg>
+            <a href="#">
+                <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Life Science Prospective Initiative</h5>
+            </a>
+            <p className="mb-3 font-normal text-gray-500 dark:text-gray-400">Life Science Prospective Initiative is a program aims to target life science aspirants in 10th grade to 12th grade of high school by raising the awareness on life sciences opportunities across the continent to ensure their right choice of path in their career.</p>
+            <a href="#" className="inline-flex items-center text-blue-600 hover:underline">
+                Exprole more here
+                <svg className="w-5 h-5 ml-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"></path><path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"></path></svg>
+            </a>
+          </div>
+          <div className="col-span-1 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <svg className="w-10 h-10 mb-2 text-gray-500 dark:text-gray-400" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5 5a3 3 0 015-2.236A3 3 0 0114.83 6H16a2 2 0 110 4h-5V9a1 1 0 10-2 0v1H4a2 2 0 110-4h1.17C5.06 5.687 5 5.35 5 5zm4 1V5a1 1 0 10-1 1h1zm3 0a1 1 0 10-1-1v1h1z" clip-rule="evenodd"></path><path d="M9 11H3v5a2 2 0 002 2h4v-7zM11 18h4a2 2 0 002-2v-5h-6v7z"></path></svg>
+            <a href="#">
+                <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">African Women in Life Science</h5>
+            </a>
+            <p className="mb-3 font-normal text-gray-500 dark:text-gray-400"> African Women in Life Science is a platform that highlights and promotes the work of African women scientists and provides support for their professional development and advancement</p>
+            <a href="#" className="inline-flex items-center text-blue-600 hover:underline">
+                Exprole more here
+                <svg className="w-5 h-5 ml-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"></path><path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"></path></svg>
+            </a>
+          </div>
+          <div className="col-span-1 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <svg className="w-10 h-10 mb-2 text-gray-500 dark:text-gray-400" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5 5a3 3 0 015-2.236A3 3 0 0114.83 6H16a2 2 0 110 4h-5V9a1 1 0 10-2 0v1H4a2 2 0 110-4h1.17C5.06 5.687 5 5.35 5 5zm4 1V5a1 1 0 10-1 1h1zm3 0a1 1 0 10-1-1v1h1z" clip-rule="evenodd"></path><path d="M9 11H3v5a2 2 0 002 2h4v-7zM11 18h4a2 2 0 002-2v-5h-6v7z"></path></svg>
+            <a href="#">
+                <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Students’ Professional development </h5>
+            </a>
+            <p className="mb-3 font-normal text-gray-500 dark:text-gray-400">Students’ Professional development is the current ongoing program that aims to enhance the participation of life science students across Africa in the scientific program including scientific conferences, research training, volunteering activities, and related internship programs.</p>
+            <a href="#" className="inline-flex items-center text-blue-600 hover:underline">
+                Exprole more here
+                <svg className="w-5 h-5 ml-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"></path><path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"></path></svg>
+            </a>
+          </div>
+          <div className="col-span-1 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <svg className="w-10 h-10 mb-2 text-gray-500 dark:text-gray-400" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5 5a3 3 0 015-2.236A3 3 0 0114.83 6H16a2 2 0 110 4h-5V9a1 1 0 10-2 0v1H4a2 2 0 110-4h1.17C5.06 5.687 5 5.35 5 5zm4 1V5a1 1 0 10-1 1h1zm3 0a1 1 0 10-1-1v1h1z" clip-rule="evenodd"></path><path d="M9 11H3v5a2 2 0 002 2h4v-7zM11 18h4a2 2 0 002-2v-5h-6v7z"></path></svg>
+            <a href="#">
+                <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">African Biosafety Network of Expertise (ABNE)</h5>
+            </a>
+            <p className="mb-3 font-normal text-gray-500 dark:text-gray-400">ABNE is a network of biosafety experts that seeks to enhance biosafety awareness, capacity, and expertise in Africa.</p>
+            <a href="#" className="inline-flex items-center text-blue-600 hover:underline">
+                Exprole more here
+                <svg className="w-5 h-5 ml-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"></path><path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"></path></svg>
+            </a>
+          </div>
+        </div>
+      </section> 
     </>
   )
 }
