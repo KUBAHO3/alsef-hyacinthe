19cfdf16f34961f347b199f65d76caa37e9ca16a
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-06-04 18:19:09 +0530

    Make navbar fixed

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,7 +8,7 @@ import nat from '../images/nat_logo.png';
 
 function Hero() {
   return (
-    <section className="dark:bg-gray-900">
+    <section className="dark:bg-gray-900"><br/><br/><br/>   
         <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
             <div className="animate-flip-up animate-once animate-ease-out animate-duration-[2000ms] mr-auto place-self-center lg:col-span-7">
                 <span className="max-w-1xl mb-4 text-2xl font-extrabold tracking-tight leading-none md:text-2xl xl:text-3xl dark:text-white" id="title">Welcome to the </span>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ function Navbar() {
   let mobileNav = ""
   mobileMenu ? mobileNav = "w-full md:block md:w-auto" : mobileNav = "hidden w-full md:block md:w-auto";
   return (
-    <nav className="border-gray-200 bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
+    <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <a href="#" className="flex items-center">
               <Image
@@ -25,9 +25,9 @@ function Navbar() {
             {mobileMenu ? "X" : <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>}
           </button>
           <div className={mobileNav} id="navbar-solid-bg">
-            <ul className="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
+            <ul className="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700" onClick={() => setMobileMenu(!mobileMenu)}>
               <li>
-                <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-pink-200 md:bg-transparent md:text-pink-700 md:p-0 md:dark:text-pink-500 dark:bg-pink-600 md:dark:bg-transparent" aria-current="page">Home</a>
+                <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-pink-200 md:bg-transparent md:text-pink-700 md:p-0 md:dark:text-pink-500 md:dark:bg-transparent" aria-current="page">Home</a>
               </li>
               <li>
                 <a href="#about" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-pink-200 md:hover:bg-transparent md:border-0 md:hover:text-pink-700 md:p-0 dark:text-white md:dark:hover:text-pink-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
